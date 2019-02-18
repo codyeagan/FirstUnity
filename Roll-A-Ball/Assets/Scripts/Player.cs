c9ae79b1169096e4b7d9b0fc4e0bdc426f28a604
codyeagan <codyeagan98@gmail.com>
2019-02-18 00:44:10 -0500

    Finished Roll-A-Ball Tutorial

--- Roll-A-Ball/Assets/Scripts/Player.cs
+++ Roll-A-Ball/Assets/Scripts/Player.cs
@@ -1,21 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 
-    public float speed = 1;
+    public float speed;
+    public Text countText;
+    public Text winText;
 
-    //move player based off input
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3(moveHorizontal, 0, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        GetComponent<Rigidbody>().AddForce(move*speed);
+        rb.AddForce(movement * speed);
+    }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
     }
 }
